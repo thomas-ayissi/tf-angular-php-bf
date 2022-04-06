69a9e8a47aeac3e7796aa4fca49628da43163783
Aude Beurive <aude.beurive@bstorm.be>
2022-04-06 10:46:33 +0200

    exo navbar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from '../models/link';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  //ou links = Link[]
+  links : Array<Link> = [
+    { title : "Accueil", url : "/"},
+    { title : "Demo", url : "/demo", children : [
+      { title : "Demo 1 - Bindings", url : "/demo/demo1" },
+      { title : "Demo 2 - Pipes", url : "/demo/demo2"},
+      { title : "Demo 3 - Les directives", url : "/demo/demo3"}
+    ]},
+    { title : "Exercices", url : "/exercice", children : [
+      { title : "Exercice 1 - Le Timer", url : "/exercice/exo1"}
+    ]}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleChildren(indice : number) : void{
+    this.links[indice].isVisible = !this.links[indice].isVisible;
+  }
 }
