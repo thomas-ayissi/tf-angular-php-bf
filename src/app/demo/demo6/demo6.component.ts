5ed69aa04adaf73836e2897999c576cdcd26c2f3
Aude Beurive <aude.beurive@bstorm.be>
2022-04-08 14:37:04 +0200

    validators + error messages

--- src/app/demo/demo6/demo6.component.ts
+++ src/app/demo/demo6/demo6.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-demo6',
@@ -11,18 +11,20 @@ export class Demo6Component implements OnInit {
   registerForm!: FormGroup;
 
   constructor(private _fb: FormBuilder) { 
-
   }
 
   ngOnInit(): void {
     this.registerForm = this._fb.group({
-      lastname: [null, []],
-      firstname: [null, []],
-      birthdate : [null, []],
-      email: [null, []],
-      password : [null, []]
+      lastname: [null, [Validators.minLength(2), Validators.maxLength(30), Validators.required]],
+      firstname: [null, [Validators.required]],
+      birthdate : [null, [Validators.required]],
+      telephone : [null, [Validators.pattern(/^(((\+|00)32[ ]?(?:\(0\)[ ]?)?)|0){1}(4(60|[789]\d)\/?(\s?\d{2}\.?){2}(\s?\d{2})|(\d\/?\s?\d{3}|\d{2}\/?\s?\d{2})(\.?\s?\d{2}){2})$/)]],
+      nombreEnfants : [null, [Validators.required, Validators.min(0), Validators.max(6)]],
+      email: [null, [Validators.required, Validators.email]],
+      password : [null, [Validators.required]]
     }
     );
+
 
   }
 
